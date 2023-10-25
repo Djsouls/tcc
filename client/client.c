3259c0afb844f8abd5eabc3faeac01d8d1d95b4f
Bernardo Schmidt Farias <bernardo.farias@neoway.com.br>
2023-10-25 18:20:24 -0300

    Update client code to use epoll and non-blocking sockets

--- client/client.c
+++ client/client.c
@@ -8,11 +8,16 @@
 
 #include <pthread.h>
 
+#include <sys/epoll.h>
+
 #include "../sockets/sockets.h"
 #include "../common/common.h"
 
 #define LATENCY_SIZE 10
 #define SLEEP_TIME 10 // 10 microsseconds
+
+#define MAX_EVENTS 10000
+#define EPOLL_TIMEOUT 3000 // 30 seconds
 
 #define MAX_CPUS 16
 
@@ -30,7 +35,7 @@ bool done = false;
 
 int main(int argc, char const* argv[])
 {
-    int core_limit = 12;
+    int core_limit = 2;
 
     pthread_t threads[MAX_CPUS];
 
@@ -57,7 +62,22 @@ int main(int argc, char const* argv[])
 }
 
 void* latency_thread(void* arg) {
+    struct epoll_event event, incoming_events[MAX_EVENTS];
+    int epoll_id, event_count;
+
+    epoll_id = epoll_create1(0);
+    if(epoll_id < 0) {
+        perror("Error crearing epoll file descriptor");
+        exit(EXIT_FAILURE);
+    }
+
     int client_fd = create_client_socket();
+    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+    event.events = EPOLLOUT;
+    event.data.fd = client_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
 
     char hello[BUFFER_SIZE];
     char hello_receive[BUFFER_SIZE];
@@ -71,19 +91,32 @@ void* latency_thread(void* arg) {
         clean_buffer(hello);
         clean_buffer(hello_receive);
 
-        memset(hello, 'b', BUFFER_SIZE);
+        event_count = epoll_wait(epoll_id, incoming_events, MAX_EVENTS, EPOLL_TIMEOUT);
 
-        begin = clock();
+        for(int i = 0; i < event_count; i++) {
+            if(incoming_events[i].events == EPOLLOUT) {
+                memset(hello, 'b', BUFFER_SIZE);
+                begin = clock();
 
-        send(client_fd, hello, strlen(hello), 0);
-        recv(client_fd, hello_receive, BUFFER_SIZE, 0);
+                send(client_fd, hello, strlen(hello), 0);
 
-        end = clock();
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            } else if(incoming_events[i].events & EPOLLIN) {
+                recv(client_fd, hello_receive, BUFFER_SIZE, 0);
 
-        if(write_to_file()) {
-            write_to_latency_file(file, begin, end);
+                end = clock();
+
+                if(write_to_file()) {
+                    write_to_latency_file(file, begin, end);
+                }
+
+                event.events = EPOLLOUT;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            }
         }
-
         usleep(SLEEP_TIME);
     }
 
@@ -97,7 +130,22 @@ void* latency_thread(void* arg) {
 }
 
 void* client_thread(void* arg) {
+   struct epoll_event event, incoming_events[MAX_EVENTS];
+    int epoll_id, event_count;
+
+    epoll_id = epoll_create1(0);
+    if(epoll_id < 0) {
+        perror("Error crearing epoll file descriptor");
+        exit(EXIT_FAILURE);
+    }
+
     int client_fd = create_client_socket();
+    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+    event.events = EPOLLOUT;
+    event.data.fd = client_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
 
     char hello[BUFFER_SIZE];
     char hello_receive[BUFFER_SIZE];
@@ -106,11 +154,25 @@ void* client_thread(void* arg) {
         clean_buffer(hello);
         clean_buffer(hello_receive);
 
-        memset(hello, 'c', BUFFER_SIZE);
+        event_count = epoll_wait(epoll_id, incoming_events, MAX_EVENTS, EPOLL_TIMEOUT);
 
-        send(client_fd, hello, strlen(hello), 0);
-        recv(client_fd, hello_receive, BUFFER_SIZE, 0); 
+        for(int i = 0; i < event_count; i++) {
+            if(incoming_events[i].events == EPOLLOUT) {
+                memset(hello, 'b', BUFFER_SIZE);
 
+                send(client_fd, hello, strlen(hello), 0);
+
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            } else if(incoming_events[i].events & EPOLLIN) {
+                recv(client_fd, hello_receive, BUFFER_SIZE, 0);
+
+                event.events = EPOLLOUT;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            }
+        }
         usleep(SLEEP_TIME);
     }
 
