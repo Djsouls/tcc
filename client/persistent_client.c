e35fa40f73696a5060cd2d01aad29e68bd82291d
Bernardo Farias <djalmirinha@gmail.com>
2023-10-26 20:43:54 -0300

    Creates new client and maintains old one
    Now one client is what we call the "persistent_client", in which the
    TCP connection is persisted and the client burst packets back and forth
    with the server
    
    The new client is based in short connectinos. Which means it creates a
    TCP connection, send and receive the packet, then closes and creates another
    one

--- client/persistent_client.c
+++ client/persistent_client.c
@@ -14,7 +14,7 @@
 #include "../common/common.h"
 
 #define LATENCY_SIZE 10
-#define SLEEP_TIME 1 // 10 microsseconds
+#define SLEEP_TIME 10 // 10 microsseconds
 
 #define MAX_EVENTS 10000
 #define EPOLL_TIMEOUT 3000 // 30 seconds
@@ -28,10 +28,6 @@ void* latency_thread(void*);
 
 bool write_to_file();
 void write_to_latency_file(FILE*, clock_t, clock_t);
-
-int create_connection(int);
-int send_request(int, int);
-int receive_request(int, int);
 
 void signal_handler(int);
 
@@ -75,29 +71,40 @@ void* latency_thread(void* arg) {
         exit(EXIT_FAILURE);
     }
 
-    FILE *file;
-    file = fopen("latency.txt", "w");
+    int client_fd = create_client_socket();
+    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+    event.events = EPOLLOUT;
+    event.data.fd = client_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
+
+    char hello[BUFFER_SIZE];
+    char hello_receive[BUFFER_SIZE];
 
     clock_t begin, end;
 
+    FILE *file;
+    file = fopen("latency.txt", "w");
+
     while(!done) {
-        int ret = create_connection(epoll_id);
-        if(ret < 0) {
-            done = true;
-        }
+        clean_buffer(hello);
+        clean_buffer(hello_receive);
 
         event_count = epoll_wait(epoll_id, incoming_events, MAX_EVENTS, EPOLL_TIMEOUT);
 
         for(int i = 0; i < event_count; i++) {
-            event = incoming_events[i];
-
-            if(event.events == EPOLLOUT) {
+            if(incoming_events[i].events == EPOLLOUT) {
+                memset(hello, 'b', BUFFER_SIZE);
                 begin = clock();
 
-                send_request(epoll_id, event.data.fd);
+                send(client_fd, hello, strlen(hello), 0);
 
-            } else if(event.events & EPOLLIN) {
-                receive_request(epoll_id, event.data.fd);
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            } else if(incoming_events[i].events & EPOLLIN) {
+                recv(client_fd, hello_receive, BUFFER_SIZE, 0);
 
                 end = clock();
 
@@ -105,7 +112,9 @@ void* latency_thread(void* arg) {
                     write_to_latency_file(file, begin, end);
                 }
 
-                close(event.data.fd);
+                event.events = EPOLLOUT;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
             }
         }
         usleep(SLEEP_TIME);
@@ -113,13 +122,15 @@ void* latency_thread(void* arg) {
 
     printf("Finishing latency thread\n");
 
+    close(client_fd);
+
     fclose(file);
 
     return 0;
 }
 
 void* client_thread(void* arg) {
-    struct epoll_event event, incoming_events[MAX_EVENTS];
+   struct epoll_event event, incoming_events[MAX_EVENTS];
     int epoll_id, event_count;
 
     epoll_id = epoll_create1(0);
@@ -128,25 +139,38 @@ void* client_thread(void* arg) {
         exit(EXIT_FAILURE);
     }
 
+    int client_fd = create_client_socket();
+    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+    event.events = EPOLLOUT;
+    event.data.fd = client_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
+
+    char hello[BUFFER_SIZE];
+    char hello_receive[BUFFER_SIZE];
 
     while(!done) {
-        int ret = create_connection(epoll_id);
-        if(ret < 0) {
-            done = true;
-        }
+        clean_buffer(hello);
+        clean_buffer(hello_receive);
 
         event_count = epoll_wait(epoll_id, incoming_events, MAX_EVENTS, EPOLL_TIMEOUT);
 
         for(int i = 0; i < event_count; i++) {
-            event = incoming_events[i];
+            if(incoming_events[i].events == EPOLLOUT) {
+                memset(hello, 'b', BUFFER_SIZE);
 
-            if(event.events == EPOLLOUT) {
-                send_request(epoll_id, event.data.fd);
+                send(client_fd, hello, strlen(hello), 0);
 
-            } else if(event.events & EPOLLIN) {
-                receive_request(epoll_id, event.data.fd);
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            } else if(incoming_events[i].events & EPOLLIN) {
+                recv(client_fd, hello_receive, BUFFER_SIZE, 0);
 
-                close(event.data.fd);
+                event.events = EPOLLOUT;
+                event.data.fd = client_fd;
+                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
             }
         }
         usleep(SLEEP_TIME);
@@ -154,49 +178,7 @@ void* client_thread(void* arg) {
 
     printf("Finishing client thread\n");
 
-    // close(client_fd);
-
-    return 0;
-}
-
-int create_connection(int epoll_id) {
-    int client_fd = create_client_socket();
-    fcntl(client_fd, F_SETFL, O_NONBLOCK);
-
-    struct epoll_event event;
-    event.events = EPOLLOUT;
-    event.data.fd = client_fd;
-
-    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
-
-    return client_fd;
-}
-
-int send_request(int epoll_id, int socket_fd) {
-    char hello[BUFFER_SIZE];
-    clean_buffer(hello);
-
-
-    memset(hello, 'b', BUFFER_SIZE);
-
-    write(socket_fd, hello, BUFFER_SIZE);
-
-    struct epoll_event event;
-    event.events = EPOLLIN;
-    event.data.fd = socket_fd;
-
-    epoll_ctl(epoll_id, EPOLL_CTL_MOD, socket_fd, &event);
-
-    return 0;
-}
-
-int receive_request(int epoll_id, int socket_fd) {
-    char hello_receive[BUFFER_SIZE];
-    clean_buffer(hello_receive);
-
-    read(socket_fd, hello_receive, BUFFER_SIZE);
-
-    epoll_ctl(epoll_id, EPOLL_CTL_DEL, socket_fd, NULL);
+    close(client_fd);
 
     return 0;
 }
