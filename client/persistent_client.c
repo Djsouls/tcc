7756d28d1c0d4362e85bc9a392af7dc4b8e9f644
Bernardo Schmidt Farias <bernardo.farias@neoway.com.br>
2023-10-28 02:11:27 -0300

    Change stuff

--- client/persistent_client.c
+++ client/persistent_client.c
@@ -26,6 +26,11 @@ void clean_buffer(char*);
 void* client_thread(void*);
 void* latency_thread(void*);
 
+int create_connection(int);
+
+int handle_send_request(int, int);
+int handle_receive_request(int, int);
+
 bool write_to_file();
 void write_to_latency_file(FILE*, clock_t, clock_t);
 
@@ -45,17 +50,18 @@ int main(int argc, char const* argv[])
     // Register signal handling for gracefully shutdown
     signal(SIGINT, signal_handler);
 
-    pthread_create(&threads[0], NULL, &latency_thread, NULL);
+    pthread_create(&threads[MAX_CPUS], NULL, &latency_thread, NULL);
 
-    for(int i = 1; i < core_limit + 1; i++) {
+    for(int i = 0; i < core_limit; i++) {
         pthread_create(&threads[i], NULL, &client_thread, NULL);
     }
 
-    pthread_join(threads[0], NULL);
-    for(int i = 1; i < core_limit + 1; i++) {
+    pthread_join(threads[MAX_CPUS], NULL);
+    for(int i = 0; i < core_limit; i++) {
         printf("Joining %i\n", i);
         pthread_join(threads[i], NULL);
     }
+
     printf("Properly exited. Bye bye\n");
 
     return 0;
@@ -130,7 +136,7 @@ void* latency_thread(void* arg) {
 }
 
 void* client_thread(void* arg) {
-   struct epoll_event event, incoming_events[MAX_EVENTS];
+    struct epoll_event incoming_events[MAX_EVENTS];
     int epoll_id, event_count;
 
     epoll_id = epoll_create1(0);
@@ -139,38 +145,24 @@ void* client_thread(void* arg) {
         exit(EXIT_FAILURE);
     }
 
-    int client_fd = create_client_socket();
-    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+    for(int i = 0; i < 4000; i++) {
+        create_connection(epoll_id);
+    }
 
-    event.events = EPOLLOUT;
-    event.data.fd = client_fd;
+    int sockid;
 
-    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
-
-    char hello[BUFFER_SIZE];
-    char hello_receive[BUFFER_SIZE];
-
+    struct epoll_event event;
     while(!done) {
-        clean_buffer(hello);
-        clean_buffer(hello_receive);
-
         event_count = epoll_wait(epoll_id, incoming_events, MAX_EVENTS, EPOLL_TIMEOUT);
 
         for(int i = 0; i < event_count; i++) {
-            if(incoming_events[i].events == EPOLLOUT) {
-                memset(hello, 'b', BUFFER_SIZE);
+            event = incoming_events[i];
+            sockid = event.data.fd;
 
-                send(client_fd, hello, strlen(hello), 0);
-
-                event.events = EPOLLIN;
-                event.data.fd = client_fd;
-                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
-            } else if(incoming_events[i].events & EPOLLIN) {
-                recv(client_fd, hello_receive, BUFFER_SIZE, 0);
-
-                event.events = EPOLLOUT;
-                event.data.fd = client_fd;
-                epoll_ctl(epoll_id, EPOLL_CTL_MOD, client_fd, &event);
+            if(event.events == EPOLLOUT) {
+                handle_send_request(epoll_id, sockid);
+            } else if(event.events & EPOLLIN) {
+                handle_receive_request(epoll_id, sockid);
             }
         }
         usleep(SLEEP_TIME);
@@ -178,7 +170,51 @@ void* client_thread(void* arg) {
 
     printf("Finishing client thread\n");
 
-    close(client_fd);
+    // close(client_fd);
+
+    return 0;
+}
+
+int create_connection(int epoll_id) {
+    int client_fd = create_client_socket();
+
+    struct epoll_event event;
+    event.events = EPOLLOUT;
+    event.data.fd = client_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_ADD, client_fd, &event);
+
+    return client_fd;
+}
+
+int handle_send_request(int epoll_id, int socket_fd) {
+    char hello[BUFFER_SIZE];
+    clean_buffer(hello);
+
+    memset(hello, 'b', BUFFER_SIZE);
+
+    send(socket_fd, hello, strlen(hello), 0);
+
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = socket_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_MOD, socket_fd, &event);
+
+    return 0;
+}
+
+int handle_receive_request(int epoll_id, int socket_fd) {
+    char hello_receive[BUFFER_SIZE];
+    clean_buffer(hello_receive);
+
+    recv(socket_fd, hello_receive, BUFFER_SIZE, 0);
+
+    struct epoll_event event;
+    event.events = EPOLLOUT;
+    event.data.fd = socket_fd;
+
+    epoll_ctl(epoll_id, EPOLL_CTL_MOD, socket_fd, &event);
 
     return 0;
 }
